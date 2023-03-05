784b7bb3b9f576e3dd7098efbd3535caa6d5841c
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-03-04 20:38:22 -0500

    created p elements to hold markers-added class and ids updated variables to reference p elem markers created var for board element

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-const topL = getElementById("top-left");
-const topC = getElementById("top-center");
-const topR = getElementById("top-right");
-const midL = getElementById("mid-left");
-const midC = getElementById("mid-center");
-const midR = getElementById("mid-right");
-const bottomL = getElementById("bottom-left");
-const bottomC = getElementById("bottom-center");
-const bottomR = getElementById("bottom-right");
+const board = document.querySelector(".board");
+const topL = document.getElementById("top-left-marker");
+const topC = document.getElementById("top-center-marker");
+const topR = document.getElementById("top-right-marker");
+const midL = document.getElementById("mid-left-marker");
+const midC = document.getElementById("mid-center-marker");
+const midR = document.getElementById("mid-right-marker");
+const bottomL = document.getElementById("bottom-left-marker");
+const bottomC = document.getElementById("bottom-center-marker");
+const bottomR = document.getElementById("bottom-right-marker");
 
+// board.addEventListener("click", handleClick(this.id));
+
+// function handleClick(id) {
+//   const clickedId = this.id;
+//   console.log(clickedId)
+// }
