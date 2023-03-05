b6aa43bbc27d18944f58db6ebe7b016a128d19ba
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-03-04 21:03:32 -0500

    Xs show up on page when clicked and do not disappear on next click

--- js/script.js
+++ js/script.js
@@ -8,10 +8,23 @@ const midR = document.getElementById("mid-right-marker");
 const bottomL = document.getElementById("bottom-left-marker");
 const bottomC = document.getElementById("bottom-center-marker");
 const bottomR = document.getElementById("bottom-right-marker");
+const cell = document.getElementsByClassName("cell");
 
-// board.addEventListener("click", handleClick(this.id));
 
-// function handleClick(id) {
-//   const clickedId = this.id;
-//   console.log(clickedId)
-// }
+// Get all cells within the board
+const cells = document.querySelectorAll('.cell');
+
+// Add click event listener to the board
+document.querySelector('.board').addEventListener('click', function(event) {
+  // Check if the clicked element is a cell
+  if (!event.target.classList.contains('cell')) return;
+  
+  // Get the ID of the clicked cell
+  const clickedCellId = event.target.id;
+  
+  // Remove the 'hide' class from the marker within the clicked cell, if it exists
+  const clickedCellMarker = document.querySelector(`#${clickedCellId}-marker`);
+  if (clickedCellMarker && clickedCellMarker.classList.contains('hide')) {
+    clickedCellMarker.classList.remove('hide');
+  }
+});
