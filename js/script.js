827a81ebb868795b9f9ed73c8e4bcd144e4ea095
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-03-04 21:55:24 -0500

    X and O alternates on clicks but box can be clicked again and symbol will change - almost there

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 const board = document.querySelector(".board");
-const topL = document.getElementById("top-left-marker");
-const topC = document.getElementById("top-center-marker");
-const topR = document.getElementById("top-right-marker");
-const midL = document.getElementById("mid-left-marker");
-const midC = document.getElementById("mid-center-marker");
-const midR = document.getElementById("mid-right-marker");
-const bottomL = document.getElementById("bottom-left-marker");
-const bottomC = document.getElementById("bottom-center-marker");
-const bottomR = document.getElementById("bottom-right-marker");
-const cell = document.getElementsByClassName("cell");
 
+let markerSymbol = "X"; // Initialize the markerSymbol to "X"
 
 // Get all cells within the board
 const cells = document.querySelectorAll('.cell');
@@ -18,13 +9,38 @@ const cells = document.querySelectorAll('.cell');
 document.querySelector('.board').addEventListener('click', function(event) {
   // Check if the clicked element is a cell
   if (!event.target.classList.contains('cell')) return;
-  
+
   // Get the ID of the clicked cell
   const clickedCellId = event.target.id;
-  
-  // Remove the 'hide' class from the marker within the clicked cell, if it exists
+  // Get the class of the clicked cell
   const clickedCellMarker = document.querySelector(`#${clickedCellId}-marker`);
+
+  // Display the correct marker (X or O)
+  document.querySelector(`#${clickedCellId}-marker`).textContent = markerSymbol;
+
+  // Remove the 'hide' class from the marker within the clicked cell, if it exists
   if (clickedCellMarker && clickedCellMarker.classList.contains('hide')) {
     clickedCellMarker.classList.remove('hide');
   }
+
+  // Set the markerSymbol to "X" on odd clicks and "O" on even clicks
+  if (markerSymbol === "X") {
+    markerSymbol = "O";
+  } else {
+    markerSymbol = "X";
+  }
 });
+
+
+
+
+
+// const topL = document.getElementById("top-left-marker");
+// const topC = document.getElementById("top-center-marker");
+// const topR = document.getElementById("top-right-marker");
+// const midL = document.getElementById("mid-left-marker");
+// const midC = document.getElementById("mid-center-marker");
+// const midR = document.getElementById("mid-right-marker");
+// const bottomL = document.getElementById("bottom-left-marker");
+// const bottomC = document.getElementById("bottom-center-marker");
+// const bottomR = document.getElementById("bottom-right-marker");
