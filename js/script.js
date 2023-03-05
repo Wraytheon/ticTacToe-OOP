6df8af7178fbd3d8a7e86c67e8379aaacd924c38
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-03-04 19:56:50 -0500

    created vars for cells

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const topL = getElementById("top-left");
+const topC = getElementById("top-center");
+const topR = getElementById("top-right");
+const midL = getElementById("mid-left");
+const midC = getElementById("mid-center");
+const midR = getElementById("mid-right");
+const bottomL = getElementById("bottom-left");
+const bottomC = getElementById("bottom-center");
+const bottomR = getElementById("bottom-right");
+
