35eabc5d77626533adcf68b9b965dc27001ffa65
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-03-07 13:19:14 -0500

    game correctly announces winner immediately!

--- js/script.js
+++ js/script.js
@@ -5,45 +5,43 @@ let markerSymbol = "X"; // Initialize the markerSymbol to "X"
 // Get all cells within the board
 const cells = document.querySelectorAll(".cell");
 
-document.querySelector(".board").addEventListener("click", playerTurns)
 document.querySelector("#start-btn").addEventListener("click", function () {
   document.querySelector(".player-one").classList.add("turn-alert");
-  document.querySelector("#players-container").classList.remove("hide")
-  document.querySelector("#board-container").classList.remove("hide")
-})
+  document.querySelector("#players-container").classList.remove("hide");
+  document.querySelector("#board-container").classList.remove("hide");
+});
 
 //* Player turns
-let currentPlayer = 'player1';
+let currentPlayer = "player1";
 
 function playerTurns() {
   // get the current player's turn
-  let playerOne = document.querySelector(".player-one")
-  let playerTwo = document.querySelector(".player-two")
+  let playerOne = document.querySelector(".player-one");
+  let playerTwo = document.querySelector(".player-two");
 
   // update the turn
-  if (currentPlayer === 'player1') {
-    playerTwo.classList.remove("turn-alert")
-    playerOne.classList.add("turn-alert")
-    currentPlayer = 'player2'
-    playerTwo.classList.add("turn-alert")
-    playerOne.classList.remove("turn-alert")
+  if (currentPlayer === "player1") {
+    playerTwo.classList.remove("turn-alert");
+    playerOne.classList.add("turn-alert");
+    currentPlayer = "player2";
+    playerTwo.classList.add("turn-alert");
+    playerOne.classList.remove("turn-alert");
   } else {
-    playerOne.classList.remove("turn-alert")
-    playerTwo.classList.add("turn-alert")
-    currentPlayer = 'player1';
-    playerOne.classList.add("turn-alert")
-    playerTwo.classList.remove("turn-alert")
+    playerOne.classList.remove("turn-alert");
+    playerTwo.classList.add("turn-alert");
+    currentPlayer = "player1";
+    playerOne.classList.add("turn-alert");
+    playerTwo.classList.remove("turn-alert");
   }
-  console.log(currentPlayer)
+  console.log(currentPlayer);
 
   // do something else
   // ...
 }
-console.log(currentPlayer)
-
+console.log(currentPlayer);
 
 // Add click event listener to the board
-document.querySelector(".board").addEventListener("click", placeMarkers)
+document.querySelector(".board").addEventListener("click", placeMarkers);
 function placeMarkers(event) {
   // Check if the clicked element is a cell
   if (!event.target.classList.contains("cell")) return;
@@ -67,6 +65,14 @@ function placeMarkers(event) {
   // Add the 'clicked' class to the clicked cell
   event.target.classList.add("clicked");
 
+  // Check if the current player has won
+  if (getWinner()) {
+    document.querySelector(
+      "#result-text"
+    ).textContent = `${markerSymbol} wins!`;
+    return;
+  }
+
   // Set the markerSymbol to "X" on odd clicks and "O" on even clicks
   if (markerSymbol === "X") {
     markerSymbol = "O";
@@ -74,10 +80,8 @@ function placeMarkers(event) {
     markerSymbol = "X";
   }
 
-  getWinner()
-};
-
-
+  playerTurns();
+}
 
 // Check if the currentPlayer has won
 function getWinner() {
@@ -97,15 +101,21 @@ function getWinner() {
   for (let combo of winningCombos) {
     let hasWon = true;
     for (let cellId of combo) {
-      if (!document.getElementById(cellId + "-marker").textContent.includes(markerSymbol)) {
-        console.log(document.getElementById(cellId + "-marker").textContent)
+      if (
+        !document
+          .getElementById(cellId + "-marker")
+          .textContent.includes(markerSymbol)
+      ) {
+        console.log(document.getElementById(cellId + "-marker").textContent);
         hasWon = false;
         break;
       }
     }
     if (hasWon) {
       // If the currentPlayer has won, display a message and disable all cells
-      document.getElementById("result-text").innerHTML = `${currentPlayer} wins`
+      document.getElementById(
+        "result-text"
+      ).innerHTML = `${currentPlayer} wins`;
       cells.forEach((cell) => {
         cell.classList.add("clicked");
       });
